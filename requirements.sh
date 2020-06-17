06afb141d44f86adbc5695e1a1784a73320ce299
Felix Muzny <muzny@colorado.edu>
2020-06-17 14:33:03 -0600

    add requirements installation script, remove installation from hardboiled setup

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,6 @@
 # ./requirements.sh
+# requirements for raspiwifi to run -- needs an internet connection to be installed
+# should either be pre-installed on image or pi needs to be connected via ethernet on first boot
 apt update
 apt install python3 python3-rpi.gpio python3-pip dnsmasq hostapd -y
 pip3 install flask pyopenssl
